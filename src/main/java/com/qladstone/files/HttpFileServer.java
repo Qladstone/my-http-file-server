79fe0dba42bac46927c0155c8844811bc98999be
Qladstone <looquanxiang@gmail.com>
2025-07-18 15:29:20 +0800

    Implement simple HTTP file server

--- src/main/java/com/qladstone/files/HttpFileServer.java
+++ src/main/java/com/qladstone/files/HttpFileServer.java
@@ -1,7 +1,30 @@
 package com.qladstone.files;
 
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.SimpleFileServer;
+
+import java.io.File;
+import java.net.InetSocketAddress;
+import java.nio.file.Path;
+
 public class HttpFileServer {
     public static void main(String[] args) {
+
         System.out.println("Running HTTP file server...");
+
+        String portProperty = System.getProperty("app.port");
+        int port = portProperty == null ? 8080 : Integer.parseInt(portProperty);
+
+        String rootPathProperty = System.getProperty("app.root");
+        String absRootPath = new File(rootPathProperty).getAbsolutePath();
+
+        System.out.printf("Serving files on port %d from directory: %s\n", port, absRootPath);
+        HttpServer fileServer = SimpleFileServer.createFileServer(
+                new InetSocketAddress(port),
+                Path.of(absRootPath),
+                SimpleFileServer.OutputLevel.INFO
+        );
+        fileServer.start();
+        System.out.printf("View files here: http://localhost:%d\n", port);
     }
 }
